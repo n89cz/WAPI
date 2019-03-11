c8473dc5f490468a965dbb016944d07a57e6fb33
n89cz <dusek@n89.cz>
2019-03-11 11:20:56 +0100

    domain registration added

---
+++ domains/wapi_domain_create.sh
@@ -0,0 +1,9 @@
+source "$(dirname $0)"/"authentication.sh"
+
+#max 1000 request per hour per user accout
+
+DOMAIN="$1"
+
+REQUEST="<request><user>$LOGIN</user><auth>$AUTH</auth><command>domain-create</command><data><name>$DOMAIN</name><period>1</period><dns></dns><owner_c>MADU89</owner_c><admin_c>MADU89</admin_c><rules><fname>Martin</fname><lname>Du
+
+curl -d $REQUEST $WAPI_URL
