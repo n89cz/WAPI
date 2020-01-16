5ddf54e2fb43c9c844c0b3863551d7d5e3b7fce7
Martin Dusek <martin.dusek@wedos.org>
2020-01-16 08:54:49 +0100

    update

--- app/configuration.sh
+++ app/configuration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #enter your WAPI login details here:
-LOGIN="dusekmartin@hotmail.com"
-PASSWORD="q9}63M>lRp8I"
+LOGIN="your@email.com"
+PASSWORD="1111111111"
 
 #WAPI URL
 WAPI_URL="https://api.wedos.com/wapi/xml"
